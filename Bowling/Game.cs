09e4c2c0bb760594d654eedb2e2b948404774270
dbhavsar <dbhavsar@tavisca.com>
2014-07-16 10:40:54 +0530

    Modification in code

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -9,13 +9,14 @@ namespace Bowling
     public class Game
     {
        
-        int score;
-        int i;
-        int[] arr = new int[20];
+        int score=0;
+        int i=0;
+        int[] arr = new int[25];
 
         public void Roll(int pins)
         {
-            if (pins == 10 && (i % 2) == 0)
+            //if (pins == 10 || (i % 2) == 0)
+            if (pins == 10 )
             {
                 arr[i] = pins;
                 i++;
@@ -32,46 +33,52 @@ namespace Bowling
 
         public int GetScore()
         {
-            //for (int j = 0; j < arr.Length; j++)
+           
             for (int j = 0; j < 20; j++)
             {
                 if (arr[j] == 10)
                 {
                     if (j % 2 == 0)
                     {
-                        if (arr[j + 2] == 10)
+                        if ((arr[j + 2]) == 10)
                         {
-                            score =score+arr[j] + arr[j + 2] + arr[j + 4];
+                            score = score + arr[j] + arr[j + 2] + arr[j + 4];
 
                         }
                         else
                         {
-                            score =score+arr[j] + arr[j + 2] + arr[j + 3];
-                        } 
+                            score = score + arr[j] + arr[j + 2] + arr[j + 3];
+                        }
+                    }
+                    else
+                    {
+                        score = score + arr[j] + arr[j + 1];
                     }
 
                 }
                 else if ((j % 2 == 0))
                 {
-                  if((arr[j] + arr[j + 1])== 10)
-                     {
-                    
-                        score =score+ arr[j] + arr[j + 1] + arr[j + 2];
+                    if ((arr[j] + arr[j + 1]) == 10)
+                    {
+                        score = score + arr[j] + arr[j + 1] + arr[j + 2];
                         j++;
-                     }
+                    }
 
                     else
                     {
                         score = score + arr[j];
                     }
                 }
-                else{
+                else
+                {
                     score = score + arr[j];
                 }
                 //throw new NotImplementedException();
             }
             return score;
         }
+
+
        
     }
 }
--- BowlingFixtures/GameFixture.cs
+++ BowlingFixtures/GameFixture.cs
@@ -6,17 +6,19 @@ namespace Bowling.BowlingFixtures
     [TestClass]
     public class GameFixture
     {
+
+        /// <summary>
+        /// In between
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void Normal()
         {
             try
             {
                 Game game = new Game();
 
-                int[] arr = { 10, 3, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-                //int[] arr = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-               // int[] arr = { 10, 10, 10, 10,10, 10, 10, 10, 10, 10,0,0};
-             //   int[] arr = { 3,7,3,7,3,7,3,7,3,7,3,7,3,7,3,7,3,7,3,7 }; 
+                int[] arr = { 10, 3, 5 };
+           
                 foreach (int i in arr)
                 {
                     game.Roll(i);
@@ -35,17 +37,19 @@ namespace Bowling.BowlingFixtures
                     , ex.GetType().Name);
             }
         }
+        /// <summary>
+        /// gutter condition all zero
+        /// </summary>
         [TestMethod]
-        public void TestMethod2()
+        public void AllZero()
         {
             try
             {
                 Game game = new Game();
 
-                //int[] arr = { 10, 3, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+             
                 int[] arr = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-                // int[] arr = { 10, 10, 10, 10,10, 10, 10, 10, 10, 10,0,0};
-               // int[] arr = { 3, 7, 3, 7, 3, 7, 3, 7, 3, 7, 3, 7, 3, 7, 3, 7, 3, 7, 3, 7 };
+             
                 foreach (int i in arr)
                 {
                     game.Roll(i);
@@ -64,6 +68,96 @@ namespace Bowling.BowlingFixtures
                     , ex.GetType().Name);
             }
         }
-     
+        /// <summary>
+        /// spare condition
+        /// </summary>
+
+        [TestMethod]
+        public void Spare()
+        {
+            try
+            {
+                Game game = new Game();
+                int[] arr = { 5, 5, 5, 5, 5, 5,5, 5,5, 5, 5,5, 5, 5, 5,5, 5, 5, 5, 5 ,5};
+                foreach (int i in arr)
+                {
+                    game.Roll(i);
+                }
+
+                int res = game.GetScore();
+
+                Assert.AreEqual(res, 150);
+
+
+            }
+
+            catch (Exception ex)
+            {
+                Assert.Fail("Caught exception"
+                    , ex.GetType().Name);
+            }
+        }
+        /// <summary>
+        /// strike condition
+        /// </summary>
+        [TestMethod]
+        public void Strike()
+        {
+            try
+            {
+                Game game = new Game();
+
+              
+                  int[] arr = { 10, 10, 10, 10,10, 10, 10, 10, 10, 10,10,10};
+             
+                foreach (int i in arr)
+                {
+                    game.Roll(i);
+                }
+
+                int res = game.GetScore();
+
+                Assert.AreEqual(res, 300);
+
+
+            }
+
+            catch (Exception ex)
+            {
+                Assert.Fail("Caught exception"
+                    , ex.GetType().Name);
+            }
+        }
+
+
+
+        [TestMethod]
+        public void StrikeSpare()
+        {
+            try
+            {
+                Game game = new Game();
+
+
+                int[] arr = { 10, 10, 10, 5, 5, 10, 10, 10, 10, 10, 10, 10, 10 };
+
+                foreach (int i in arr)
+                {
+                    game.Roll(i);
+                }
+
+                int res = game.GetScore();
+
+                Assert.AreEqual(res, 275);
+
+
+            }
+
+            catch (Exception ex)
+            {
+                Assert.Fail("Caught exception"
+                    , ex.GetType().Name);
+            }
+        }
     }
 }
